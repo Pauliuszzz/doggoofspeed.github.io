80e04dc5bfad9c75375e79e5c9eaeb3fa7a75c01
DoggoOfSpeed <doggoofspeed@gmail.com>
2020-02-20 19:47:49 +0200

    Optimisation

--- js/WorldOne.js
+++ js/WorldOne.js
@@ -1,114 +1,45 @@
 randomNumber = Math.floor(Math.random()*10+1);
 
 window.onload = function() {
+	document.getElementById("One").style.display = "none";
+	document.getElementById("Two").style.display = "none";
+	document.getElementById("Three").style.display = "none";
+	document.getElementById("Four").style.display = "none";
+	document.getElementById("Five").style.display = "none";
+	document.getElementById("Six").style.display = "none";
+	document.getElementById("Seven").style.display = "none";
+	document.getElementById("Eight").style.display = "none";
+	document.getElementById("Nine").style.display = "none";
+	document.getElementById("Ten").style.display = "none";
+	
 	if (randomNumber == 1) {
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("One").style.display = "block";
 	}
 	if (randomNumber == 2) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Two").style.display = "block";
 	}
 	if (randomNumber == 3) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Three").style.display = "block";
 	}
 	if (randomNumber == 4) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Four").style.display = "block";
 	}
 	if (randomNumber == 5) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Five").style.display = "block";
 	}
 	if (randomNumber == 6) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Six").style.display = "block";
 	}
 	if (randomNumber == 7) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Seven").style.display = "block";
 	}
 	if (randomNumber == 8) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Eight").style.display = "block";
 	}
 	if (randomNumber == 9) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Ten").style.display = "none";
+	document.getElementById("Nine").style.display = "block";
 	}
 	if (randomNumber == 10) {
-		document.getElementById("One").style.display = "none";
-		document.getElementById("Two").style.display = "none";
-		document.getElementById("Three").style.display = "none";
-		document.getElementById("Four").style.display = "none";
-		document.getElementById("Five").style.display = "none";
-		document.getElementById("Six").style.display = "none";
-		document.getElementById("Seven").style.display = "none";
-		document.getElementById("Eight").style.display = "none";
-		document.getElementById("Nine").style.display = "none";
+	document.getElementById("Ten").style.display = "block";
 	}
 }
---
+++ js/WorldOne1.js
@@ -0,0 +1,114 @@
+randomNumber = Math.floor(Math.random()*10+1);
+
+window.onload = function() {
+	if (randomNumber == 1) {
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 2) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 3) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 4) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 5) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 6) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 7) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 8) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 9) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Ten").style.display = "none";
+	}
+	if (randomNumber == 10) {
+		document.getElementById("One").style.display = "none";
+		document.getElementById("Two").style.display = "none";
+		document.getElementById("Three").style.display = "none";
+		document.getElementById("Four").style.display = "none";
+		document.getElementById("Five").style.display = "none";
+		document.getElementById("Six").style.display = "none";
+		document.getElementById("Seven").style.display = "none";
+		document.getElementById("Eight").style.display = "none";
+		document.getElementById("Nine").style.display = "none";
+	}
+}
