ba058da47a52668f0a8eee62f1076b229bfba6df
DoggoOfSpeed <doggoofspeed@gmail.com>
2020-02-24 19:26:27 +0200

    Huge rework

---
+++ js/Onload.js
@@ -0,0 +1,4 @@
+window.onload = function onload() {
+	document.getElementById("ButtonThree").disabled = true;
+	document.getElementById("reply-text").style.display = "none";
+}
---
+++ js/Scenarios.js
@@ -0,0 +1,96 @@
+questions = [
+	{
+		"question": "It was a calm afternoon when you stumbled upon a crashed airplane. You can search the inside (1), or try to power it up (2)."
+	},
+	{
+		"question": "After walking for 10 minutes you meet Bob the Kitten. You can give him some milk (1), or kill him (2)."
+	},
+	{
+		"question": "You were calmly walking, until a bandit jumped you. You woke up tied up. You can shout for help (1), or try to flatter him (2)."
+	},
+	{
+		"question": "You were travelling at night, when a shooting star appeared. You can think of a wish (1), or continue walking (2)."
+	},
+	{
+		"question": "It was a rainy day when you saw an abandoned wagon and decided to hide there for a bit. You found a frog there. You can kiss it (1), or eat it (2)."
+	},
+	{
+		"question": "The sun is setting and you want to sleep. You can try to sleep in a tree (1), or on a beach (2)."
+	},
+	{
+		"question": "You find a strange seemingly abandoned house. You can knock on the door (1), or just go through (2)."
+	},	
+	{
+		"question": "You find a doll on the road. You can take it (1), or leave it (2)."
+	},	
+	{
+		"question": "While walking you encounter Antaniukas - the car model hobbyist. You can talk to him (1), or compliment him (2)."
+	},	
+	{
+		"question": "You were walking through a deep forest, when you met Paulius the Tiger. You can shout at him (1), or feed him (2)"
+	}
+];
+replies1 = [
+	{
+		"reply": "You find a secret  compartment that contains some coke. Seems like a win to me!"
+	},
+	{
+		"reply": "The kitten was quite happy, since the last time he drank milk, was when his mom still fed him."
+	},
+	{
+		"reply": "You started shouting for help. Lucky you, the bandit's name was Help. He got scared, thinking you are a magician, and let you go."
+	},
+	{
+		"reply": "While you were thinking of a wish the star fell down...<br>...on your head, it started bleeding."
+	},
+	{
+		"reply": "You kissed it and nothing happened. Such a surprise!"
+	},
+	{
+		"reply": "You start sleeping on a tree, but end up sleeping on the ground. Congrats - you now have a headache!"
+	},
+	{
+		"reply": "You knock on the door, unfortunately wolves hear the knock and attack you. You are injured and lucky to be alive."
+	},
+	{
+		"reply": "You take the doll. A cursed doll. Congratulations! Now you are unable to have a good rest!"
+	},
+	{
+		"reply": "You start talking, he tells you his deepest desires. He starts talking so enthusiastically, he starts crying over the fact that he doesn't have \"Ford Focus\"."
+	},
+	{
+		"reply": "You start shouting and he starts crying, runs back home."
+	}
+];
+replies2 = [
+	{
+		"reply": "You try to power it up, but the badly damaged engine blows up."
+	},
+	{
+		"reply": "You attacked, but he was ready. 20 other kittens jumped you and music started playing:<br>Everybody was kung fu fighting, (ooogh, aaagh).<br>Those CATS were fast as lightning (ooogh, aaagh).<br>It was very very frightening, (ooogh, aaagh).<br>but they fought with expert timing (ooogh, aaagh)."
+	},
+	{
+		"reply": "You tried to flatter him, but the bandit was highly depressed, and misstook your compliments as irony. He was so angry that he didn't even see how you ran away."
+	},
+	{
+		"reply": "You continued walking and nothing happened."
+	},
+	{
+		"reply": "You eat it and become french. Why? Because the french eat frogs, and my sense of humour is trash."
+	},
+	{
+		"reply": "You get woken up by the waves that tried to choke you. You are fairly well rested"
+	},
+	{
+		"reply": "You open the doors and find some antidepressants. You can finally feel happy for an hour."
+	},
+	{
+		"reply": "You left it. It seems like it was a voodoo doll because 2 hours later your whole body started aching. Most likely somebody ran over it."
+	},
+	{
+		"reply": "He was so happy, because somebody was actually nice to him."
+	},
+	{
+		"reply": "You give him some meat, sadly he eats not only the meat, but your arm too!"
+	}
+];
