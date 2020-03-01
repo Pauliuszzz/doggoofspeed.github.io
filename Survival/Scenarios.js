e7950b8f1eaa45998792de3b5248149bcde8732a
DoggoOfSpeed <doggoofspeed@gmail.com>
2020-03-01 16:09:45 +0200

    Update 0.2

--- Survival/Scenarios.js
+++ Survival/Scenarios.js
@@ -4,149 +4,194 @@ scenarios = [
 		"scenario": "It was a calm evening when you stumbled upon a crashed airplane. You can search the insides (1), try to power it up (2), or use it as a bed (3).",
 		"reply1": "You find a secret  compartment that contains some Coke. Seems like a win to me!",
 		"score1": 1000,
-		"reply2": "You try to power it up, but the badly damaged engine blows up.",
+		"death1": 0,
+		"reply2": "You try to power it up, but the badly damaged engine blows up. You die.",
 		"score2": 0,
+		"death2": 1,
 		"reply3": "It started raining in the midnight, so I guess staying in the plane was definitely worth it,",
-		"score3": 1000
+		"score3": 1000,
+		"death3": 0
 	},
 	{
 		//Bob the Kitten
 		"scenario": "After walking for 10 minutes you meet Bob the Kitten. You can give him some milk (1), try to kill him (2) or tease him with that cheap laser you bought a few days ago (3).",
 		"reply1": "The kitten was quite angry at you for even mentioning milk. Apparently, he was a vegan and milk is a no go for him",
 		"score1": 250,
-		"reply2": "You attacked, but he was ready. 20 other kittens jumped you and music started playing:<br>Everybody was kung fu fighting, (ooogh, aaagh).<br>Those CATS were fast as lightning (ooogh, aaagh).<br>It was very very frightening, (ooogh, aaagh).<br>but they fought with expert timing (ooogh, aaagh).",
+		"death1": 0,
+		"reply2": "You attacked, but he was ready. 20 other kittens jumped you and music started playing:<br>Everybody was kung fu fighting, (ooogh, aaagh).<br>Those CATS were fast as lightning (ooogh, aaagh).<br>It was very very frightening, (ooogh, aaagh).<br>but they fought with expert timing (ooogh, aaagh).<br>You were killed by the gang of kittens.",
 		"score2": 0,
+		"death2": 1,
 		"reply3": "Bob was quick, but the laser beam was quicker. He stopped and started crying for being the failure that he is.",
-		"score3": 750
+		"score3": 750,
+		"death3": 0
 	},
 	{
 		//Bandit
-		"scenario": "You were calmly walking until a bandit jumped you. You woke up tied up. You can shout for help (1), try to flatter him (2), or invite him to burn some marshmallows above a bonfire and talk about the bandit's mental health problems(3).",
+		"scenario": "You were calmly walking until a bandit jumped you. You woke up tied up. You can shout for help (1), try to flatter him (2), or invite him to burn some marshmallows above a bonfire and talk about the bandit's mental health problems (3).",
 		"reply1": "You started shouting for help. Lucky you, the bandit's name was Help. He got scared, thinking you are a magician and let you go.",
 		"score1": 1000,
-		"reply2": "You tried to flatter him, but the bandit was highly depressed and mistook your compliments as irony. He was so angry that he tried to end your life on the spot, but rage slowed his reflexes and you ran away.",
+		"death1": 0,
+		"reply2": "You tried to flatter him, but the bandit was highly depressed and mistook your compliments as irony. He was so angry that he tried to end your life on the spot.",
 		"score2": 0,
+		"death2": 1,
 		"reply3": "While you had a good idea, you didn't have marshmallows, so when he untied you hoping to have a nice evening, you broke his heart and ran away...",
-		"score3": 500
+		"score3": 500,
+		"death3": 0
 	},
 	{
 		//Shooting Star
 		"scenario": "You were traveling at night when a shooting star appeared. You can stop and think of a wish (1), run for your life in case it is a meteorite (2), or just continue walking (3).",
-		"reply1": "While you were thinking of a wish the star fell down...<br>...on your head, it started bleeding.",
+		"reply1": "While you were thinking of a wish the star fell down...<br>...on your head. The impact was so strong, that you died.",
 		"score1": 0,
+		"death1": 1,
 		"reply2": "You were lucky, since 5 minutes after running you heard something very loud behind you. Oh? What's that? Is that not the sound of a meteorite? Well, you turned around and saw a huge bear, so now I can assure you that that, in fact, was not a flying rock. You played dead, although you almost didn't have to.",
 		"score2": 0,
+		"death2": 0,
 		"reply3": "Since you wanted to continue walking, that was exactly what you did. Hey, who said dreams couldn't be true!",
-		"score3": 1000
+		"score3": 1000,
+		"death3": 0
 	},
 	{
 		//Frog
 		"scenario": "It was a rainy day when you saw an abandoned wagon and decided to hide there for a bit. You found a frog there. You can kiss it (1), eat it (2), or throw out it of the wagon (3).",
 		"reply1": "You kissed it and nothing happened. Such a surprise!",
 		"score1": 500,
-		"reply2": "You eat it and become french. Why? Because the french eat frogs would be my guess.",
-		"score2": 1000,
-		"reply3": "The frog was very angry, but in the end, it was just a frog, and couldn't do anything.",
-		"score3": 250
+		"death1": 0,
+		"reply2": "You eat it and die. Why? Because the frog was toxic would be my guess.",
+		"score2": 0,
+		"death2": 1,
+		"reply3": "The frog was very angry, but in the end it was just a frog, and couldn't do anything.",
+		"score3": 250,
+		"death3": 0
 	},
 	{
 		//Sleep
-		"scenario": "The sun is setting and you want to sleep. You can try to sleep in a tree (1), on a beach (2), or you, of course, could just stay awake all night (3).",
+		"scenario": "The sun is setting and you want to sleep. You can try to sleep in a tree (1), on a beach (2), or you of course could just stay awake all night (3).",
 		"reply1": "Sleeping in a tree isn't the most comfortable thing in the world, but at least it is safe.",
 		"score1": 500,
-		"reply2": "You get woken up by the waves that tried to choke you. You are as rested as you could be, although that doesn't say much.",
-		"score2": 1000,
+		"death1": 0,
+		"reply2": "The waves choke you to death.",
+		"score2": 0,
+		"death2": 1,
 		"reply3": "Staying awake all night is usually not good for your health and sleep schedule and this time is no different.",
-		"score3": 0
+		"score3": 0,
+		"death3": 0
 	},
 	{
 		//Abandoned House
 		"scenario": "You find a strange, seemingly abandoned house. You can knock on the door (1), just go through (2), or run in guns blazing (3).",
-		"reply1": "You knock on the door, unfortunately, wolves hear the knock and attack you. You are injured and lucky to be alive.",
+		"reply1": "You knock on the door, unfortunately wolves hear the knock and attack you. You are as alive as a dead person.",
 		"score1": 0,
+		"death1": 1,
 		"reply2": "The door is locked (for safety reasons of course).",
 		"score2": 500,
+		"death2": 0,
 		"reply3": "You didn't have guns and the best alternative was a kitchen knife, but you still managed to break the door. Inside you found some antidepressants. You can finally feel happy for an hour!",
-		"score3": 1000
+		"score3": 1000,
+		"death3": 0
 	},	
 	{
 		//Doll
 		"scenario": "You find a doll on the road. You can take it (1), leave it (2), or you can put it aside so no one runs over it (3).",
-		"reply1": "You take the doll. Congratulations! You now have a doll. With that dollish doll, you can do a lot of stuff that people usually do with dollish dolls that are dolls.",
+		"reply1": "You take the doll. Congratulations! You now have a doll. With that dollish doll you can do a lot of stuff that people usually do with dollish dolls that are dolls.",
 		"score1": 1000,
+		"death1": 0,
 		"reply2": "You left it. It seems like it was a voodoo doll because 2 hours later your whole body started aching. Most likely somebody ran over it.",
 		"score2": 0,
-		"reply3": "You put aside, and no one ran over it. It didn't benefit you, but at least it was less dirty when the girl who was leading her father to hang took it.",
-		"score3": 500
+		"death2": 0,
+		"reply3": "Apparently the doll was linked with a trap. After you moved the doll a cloud of arrows came from the bushes and killed you.",
+		"score3": 500,
+		"death3": 1
 	},	
 	{
 		//Antaniukas
 		"scenario": "While walking you encounter Antaniukas - the car model hobbyist. You can talk to him (1), ask where his parents are (2), or give him a car toy (3).",
 		"reply1": "You start talking, he tells you his deepest desires. He talks so enthusiastically, he starts crying over the fact that he doesn't have a \"Ford Focus\".",
 		"score1": 750,
-		"reply2": "He said that they died in a car accident. Apparently, that was the reason he became a car collector.",
+		"death1": 0,
+		"reply2": "He said that they died in a car accident. Apparently that was the reason he became a car collector.",
 		"score2": 250,
+		"death2": 0,
 		"reply3": "He refused to take the toy and gave me an hour-long explanation between car models and car toys",
-		"score3": 0
+		"score3": 0,
+		"death3": 0
 	},	
 	{
 		//Paulius the Tiger
-		"scenario": "You were walking through a deep forest when you met Paulius the Tiger. You can shout at him (1), feed him (2), or play with him (3).",
+		"scenario": "You were walking through a deep forest, when you met Paulius the Tiger. You can shout at him (1), feed him (2), or play with him (3).",
 		"reply1": "You shout, he starts crying and runs back home. Was it worth it? Probably no, but now you can brag that you defeated a tiger.",
 		"score1": 750,
-		"reply2": "You gave him some meat, sadly he eat not only the meat but your arm too!",
+		"death1": 0,
+		"reply2": "You gave him some meat, sadly he eat not only the meat, but your arm too!",
 		"score2": 0,
-		"reply3": "Tigers understand the word \"Playing\" as biting and fighting, so this really didn't end well for you.",
-		"score3": 0
+		"death2": 0,
+		"reply3": "Tigers understand the word \"Playing\" as biting and fighting, so this realy didn't end well for you.",
+		"score3": 0,
+		"death3": 1
 	},
 	{
 		//Circus
 		"scenario": "You were sleeping when you overheard festive music. After going to the source, you found a traveling circus. You can go to the popcorn stand (1), talk to a clown (2), or inspect the zebra (3).",
 		"reply1": "After going to the popcorn stand you find out it's closed since it is midnight after all. The stand is painted in dazzling colors. If it wasn't next to a circus, it would really <i>pop</i> out.",
 		"score1": 1000,
+		"death1": 0,
 		"reply2": "You go to the clown, but he doesn't respond. He just watches the sunset smiling with his freakish smile.",
 		"score2": 250,
-		"reply3": "You go there and stroke it. After you're done, you realize that your palms are pitch black and the so-called zebra is a white horse in disguise.",
-		"score3": 750
+		"death2": 0,
+		"reply3": "You go there and to stroke it. Sadly the zebra wasn't in a very good mood and kicked you so hard you died.",
+		"score3": 750,
+		"death3": 1
 	},
 	{
 		//Stasiukas the Donkey
 		"scenario": "While walking through a quite densely populated area you hear a donkey. A donkey who is screeching. You can help it (1), take it for yourself (2), or put another donkey inside (3)",
 		"reply1": "Apparently the donkey was stuck in a hole where one of the townspeople was going to be buried. After you help him, you investigate its tag. Apparently, he is the Stasiukas - the town mayor's personal donkey. Such a weird town this is...",
 		"score1": 750,
-		"reply2": "While you were pleased with your finding, the town's guards weren't. Apparently, this wasn't an ordinary animal. He was Stasiukas - the town mayor's personal donkey. Thankfully the guards thought that bringing Stasiukas back to his home was more important than catching you.",
+		"death1": 0,
+		"reply2": "While you were pleased with your finding, the town's guards weren't. Apparently, this wasn't an ordinary animal. He was Stasiukas - the town mayor's personal donkey. You tried to run, but the guard's arrows were faster than you. You died.",
 		"score2": 0,
+		"death2": 1,
 		"reply3": "You put it inside, grab some popcorn, and watch the two battle it out. It's animal cruelty, but no one else sees it, so it's fine.",
-		"score3": 1000
+		"score3": 1000,
+		"death3": 0
 	},
 	{
 		//Poppy field
 		"scenario": "Out of nowhere a poppy field appeared. It wouldn't be a problem if your path went around it. But no, it went straight through the middle of this mesmerizing field. You can try your luck and go straight through (1), just walk around it (2), or just turn around and go back (3).",
 		"reply1": "After 10 minutes of going through this red sea of flowers, you are safely back on the road. Now you even have some sleeping pills - poppies.",
 		"score1": 1000,
+		"death1": 0,
 		"reply2": "When walking you hear something behind the bushes (probably wolves). Oh no. OH NO. <b>OH FUCK!</b>.",
 		"score2": 0,
+		"death2": 1,
 		"reply3": "You turn around and go back until you find an intersection. You choose to go some other way and skip the poppy field.",
-		"score3": 500
+		"score3": 500,
+		"death3": 0
 	},
 	{	
 		//Sweet House
 		"scenario": "While walking through a forest you find a rather sweet looking house. It may or may not be made out of candy. You can knock on the door (1), try to eat the house (2), or summon a black hole to remove this house from the face of the earth (3).",
 		"reply1": "The door opens and you meet a rather old black woman who can only say \"Sweetie\" for some reason. She gives you some cookies, and wishes you luck.",
 		"score1": 1000,
+		"death1": 0,
 		"reply2": "You try to eat it, but it's wood. I know, such a surprise, such a plot twist. A house that isn't made out of candy? Outrageous! Why would anyone with a grain of brain make a house out of candy? Anyway, your mouth hurts now.",
 		"score2": 0,
+		"death2": 0,
 
-		"score3": 666
+		"score3": 666,
+		"death3": 1
 	},
 	{	
 		//Jonas the coke addict
 		"scenario": "You find a house with an interesting inhabitant. That's right! It's Jonas - the soda addict! You can give him a soda bottle filled with dirty water (1), more soda (2), or you could just talk to him about soda.",
 		"reply1": "He chugs the bottle in one big gulp and doesn't even taste the difference. He even asks for more...",
 		"score1": 500,
+		"death1": 0,
 		"reply2": "He takes the bottles like a brain dead child and happily drinks them. While drinking he tells you he may or may not have diabetes. Oh, and you really don't want to mix diabetes and a lot of sugar...<br>And... He's dead. Welp gotta leave before anyone finds you.",
 		"score2": 0,
+		"death2": 0,
 		"reply3": "After talking with him for 5 minutes you are sure that he is a <i>noob</i>, who doesn't even know he's a noob who doesn't even know all the chemical compounds inside the most popular modern-day fizzy drinks.",
-		"score3": 1000
+		"score3": 1000,
+		"death3": 0
 	}
 ];
