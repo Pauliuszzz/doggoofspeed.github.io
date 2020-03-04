ad6489dd9b1bb02d1fbf42d963ff2643903e8749
DoggoOfSpeed <doggoofspeed@gmail.com>
2020-03-04 19:50:15 +0200

    0.2.2.1

--- js/Onload.js
+++ js/Onload.js
@@ -3,10 +3,19 @@ window.onload = function onload() {
 	document.getElementById("ButtonFive").style.display = "none"
 	document.getElementById("reply-text").style.display = "none";
 	if(window.innerWidth < 600){
-	document.getElementById("ButtonOne").classList.add('btn-sm')
-	document.getElementById("ButtonTwo").classList.add('btn-sm')
-	document.getElementById("ButtonThree").classList.add('btn-sm')
-	document.getElementById("ButtonFour").classList.add('btn-sm')
-	document.getElementById("ButtonFive").classList.add('btn-sm')
+	document.getElementById("ButtonOne").classList.add("mobile", "btn-sm")
+	document.getElementById("ButtonTwo").classList.add("mobile", "btn-sm")
+	document.getElementById("ButtonTwo").classList.add("mobile", "btn-sm")
+	document.getElementById("ButtonThree").classList.add("mobile", "btn-sm")
+	document.getElementById("ButtonFour").classList.add("mobile", "btn-sm")
+	document.getElementById("ButtonFive").classList.add("mobile", "btn-sm")
+	}
+	if(window.innerWidth > 600){
+	document.getElementById("ButtonOne").classList.add('desktop')
+	document.getElementById("ButtonTwo").classList.add('desktop')
+	document.getElementById("ButtonTwo").classList.add('desktop')
+	document.getElementById("ButtonThree").classList.add('desktop')
+	document.getElementById("ButtonFour").classList.add('desktop')
+	document.getElementById("ButtonFive").classList.add('desktop')
 	}
 }
