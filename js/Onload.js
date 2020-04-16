16f9a4b3e5e8eb09f8feef042a596a5ee1f7ce6e
DoggoOfSpeed <doggoofspeed@gmail.com>
2020-04-16 10:59:50 +0300

    0.5.0

--- js/Onload.js
+++ js/Onload.js
@@ -2,18 +2,11 @@ window.onload = function onload() {
 	$("#ButtonFour").prop('disabled', true);
 	$("#reply-text").hide()
 	if(window.innerWidth < 576){
-	$("#ButtonOne").addClass("mobile btn-sm")
-	$("#ButtonTwo").addClass("mobile btn-sm")
-	$("#ButtonTwo").addClass("mobile btn-sm")
-	$("#ButtonThree").addClass("mobile btn-sm")
-	$("#ButtonFour").addClass("mobile btn-sm")
-	}
-	if(window.innerWidth > 576){
-	$("#ButtonOne").addClass("desktop")
-	$("#ButtonTwo").addClass("desktop")
-	$("#ButtonTwo").addClass("desktop")
-	$("#ButtonThree").addClass("desktop")
-	$("#ButtonFour").addClass("desktop")
+	$("#ButtonOne").addClass("btn-sm")
+	$("#ButtonTwo").addClass("btn-sm")
+	$("#ButtonTwo").addClass("btn-sm")
+	$("#ButtonThree").addClass("btn-sm")
+	$("#ButtonFour").addClass("btn-sm")
 	}
 	var i = scenarios.length, k , temp;
 	while(--i > 0){
