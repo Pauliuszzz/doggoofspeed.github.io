0f437547d2690e89928a0c49062879dc37b2909c
Doggo <doggoofspeed@gmail.com>
2021-03-18 08:31:46 +0200

    add build.py

---
+++ build.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import os.path
+
+
+# Insert the location of index.html
+exportPath = os.path.expanduser("~") + "/Downloads/Project_Passage"
+
+if (os.path.isdir(exportPath)):
+    shutil.rmtree(exportPath)
+os.mkdir(exportPath)
+shutil.copytree('Survival', exportPath + '/Survival')
+shutil.copytree('Story', exportPath + '/Story')
+shutil.copytree('resources', exportPath + '/resources')
+shutil.copytree('js', exportPath + '/js')
+shutil.copytree('css', exportPath + '/css')
+shutil.copytree('Arcade', exportPath + '/Arcade')
+shutil.copy2('index.html', exportPath)
